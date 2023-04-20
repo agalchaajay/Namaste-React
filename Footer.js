15444897d0b17b757d4ca6e983286ee3116a6777
_ajayagalcha <130730585+agalchaajay@users.noreply.github.com>
2023-04-20 11:11:15 +0530

    Footer Added

---
+++ Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="item2">
+          <span style={{ paddingRight: 5 }}>Copyright </span>
+
+          <span style={{ paddingLeft: 5 }}>
+            {new Date().getFullYear()} YourCompany. All Rights Reserved.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
