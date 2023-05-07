b850418f9dce29b8b68e3b5d8edef8d99689d1a9
_ajayagalcha <130730585+agalchaajay@users.noreply.github.com>
2023-05-07 09:16:45 +0530

    restructure in component level

--- App.js
+++ App.js
@@ -1,206 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Footer from "./Footer";
+import Footer from "./src/components/Footer";
+import Header from "./src/components/Header";
+import Body from "./src/components/Body";
 
-const Header = () => {
-  return (
-    <div className="header-Container">
-      <div className="logo-main">
-        <img
-          className="logo"
-          src="https://e0.pxfuel.com/wallpapers/812/478/desktop-wallpaper-food-delivery-app-icon-ios-icon-app-icon-design.jpg"
-        ></img>
-      </div>
-      <div className="Nav-item">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const RestaurantCard = (props) => {
-  console.log(props);
-  const resData = props.resData;
-  console.log(resData.resName);
-  return (
-    <div className="restaurant-card">
-      <div className="card-container">
-        <img
-          className="restaurant-img"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2LZTRb_-b5ixB1fLsXggzz9Pw-8HfWbZcHST58stAdQ&usqp=CAU&ec=48600113"
-        ></img>
-        <div className="card-content">
-          <h2>{resData.resName}</h2>
-          <h5>{resData.cuisines}</h5>
-          <h4>
-            {resData.rating}
-            <img
-              className="rating-image"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKX2acAb9D9lgQw0_CX05PONEAIqIKRxlRF3la9ULcSQ&usqp=CAU&ec=48600113"
-            ></img>
-          </h4>
-          <h4>{resData.time}</h4>
+const RootApp = (prop) => {
+  const condn = prop.condn;
+  if (condn) {
+    return (
+      <div className="Main">
+        <div className="Header">
+          <Header />
         </div>
+        <Body />
+        <Footer />
       </div>
-    </div>
-  );
+    );
+  }
+  return <Body />;
 };
 
-const resList = [
-  {
-    resName: "Meghana Foods",
-    cuisines: "North Indian, Biryani,....",
-    rating: 4.4,
-    time: 35,
-  },
-  {
-    resName: "KFC",
-    cuisines: "Burger, Biryani,....",
-    rating: 4.3,
-    time: 30,
-  },
-  {
-    resName: "Punjabi Foods",
-    cuisines: "North Indian, Home Food....",
-    rating: 4.2,
-    time: 25,
-  },
-  {
-    resName: "Roti wala",
-    cuisines: "North Thali, Home Food,....",
-    rating: 4.1,
-    time: 38,
-  },
-  {
-    resName: "Biryani Foods",
-    cuisines: "Biryani,....",
-    rating: 3.4,
-    time: 50,
-  },
-  {
-    resName: "Meghana Foods",
-    cuisines: "North Indian, Biryani,....",
-    rating: 4.4,
-    time: 35,
-  },
-  {
-    resName: "KFC",
-    cuisines: "Burger, Biryani,....",
-    rating: 4.3,
-    time: 30,
-  },
-  {
-    resName: "Punjabi Foods",
-    cuisines: "North Indian, Home Food....",
-    rating: 4.2,
-    time: 25,
-  },
-  {
-    resName: "Roti wala",
-    cuisines: "North Thali, Home Food,....",
-    rating: 4.1,
-    time: 38,
-  },
-  {
-    resName: "Biryani Foods",
-    cuisines: "Biryani,....",
-    rating: 3.4,
-    time: 50,
-  },
-  {
-    resName: "Meghana Foods",
-    cuisines: "North Indian, Biryani,....",
-    rating: 4.4,
-    time: 35,
-  },
-  {
-    resName: "KFC",
-    cuisines: "Burger, Biryani,....",
-    rating: 4.3,
-    time: 30,
-  },
-  {
-    resName: "Punjabi Foods",
-    cuisines: "North Indian, Home Food....",
-    rating: 4.2,
-    time: 25,
-  },
-  {
-    resName: "Roti wala",
-    cuisines: "North Thali, Home Food,....",
-    rating: 4.1,
-    time: 38,
-  },
-  {
-    resName: "Biryani Foods",
-    cuisines: "Biryani,....",
-    rating: 3.4,
-    time: 50,
-  },
-  {
-    resName: "Meghana Foods",
-    cuisines: "North Indian, Biryani,....",
-    rating: 4.4,
-    time: 35,
-  },
-  {
-    resName: "KFC",
-    cuisines: "Burger, Biryani,....",
-    rating: 4.3,
-    time: 30,
-  },
-  {
-    resName: "Punjabi Foods",
-    cuisines: "North Indian, Home Food....",
-    rating: 4.2,
-    time: 25,
-  },
-  {
-    resName: "Roti wala",
-    cuisines: "North Thali, Home Food,....",
-    rating: 4.1,
-    time: 38,
-  },
-  {
-    resName: "Biryani Foods",
-    cuisines: "Biryani,....",
-    rating: 3.4,
-    time: 50,
-  },
-];
-const Body = () => {
-  return (
-    <div>
-      <div className="search-item">
-        <input placeholder="Search here...."></input>
-        <button className="search-btn"> Search</button>
-      </div>
-      <div className="restaurantcard-main">
-        <RestaurantCard resData={resList[0]} />
-        {resList.map((restaurant, index) => (
-          <RestaurantCard key={index} resData={restaurant} />
-        ))}
-      </div>
-    </div>
-  );
+export const searchCard = (x, event) => {
+  alert(x + " Great Shot!" + event.type);
 };
 
-const RootApp = () => {
-  return (
-    <div className="Main">
-      <div className="Header">
-        <Header />
-      </div>
-      <Body />
-      <Footer />
-    </div>
-  );
-};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<RootApp />);
+root.render(<RootApp condn={true} />);
---
+++ src/utils/CardData.js
@@ -0,0 +1,124 @@
+resList = [
+  {
+    resName: "Meghana Foods",
+    cuisines: "North Indian, Biryani,....",
+    rating: 4.4,
+    time: 35,
+  },
+  {
+    resName: "KFC",
+    cuisines: "Burger, Biryani,....",
+    rating: 4.3,
+    time: 30,
+  },
+  {
+    resName: "Punjabi Foods",
+    cuisines: "North Indian, Home Food....",
+    rating: 4.2,
+    time: 25,
+  },
+  {
+    resName: "Roti wala",
+    cuisines: "North Thali, Home Food,....",
+    rating: 4.1,
+    time: 38,
+  },
+  {
+    resName: "Biryani Foods",
+    cuisines: "Biryani,....",
+    rating: 3.4,
+    time: 50,
+  },
+  {
+    resName: "Meghana Foods",
+    cuisines: "North Indian, Biryani,....",
+    rating: 4.4,
+    time: 35,
+  },
+  {
+    resName: "KFC",
+    cuisines: "Burger, Biryani,....",
+    rating: 4.3,
+    time: 30,
+  },
+  {
+    resName: "Punjabi Foods",
+    cuisines: "North Indian, Home Food....",
+    rating: 4.2,
+    time: 25,
+  },
+  {
+    resName: "Roti wala",
+    cuisines: "North Thali, Home Food,....",
+    rating: 4.1,
+    time: 38,
+  },
+  {
+    resName: "Biryani Foods",
+    cuisines: "Biryani,....",
+    rating: 3.4,
+    time: 50,
+  },
+  {
+    resName: "Meghana Foods",
+    cuisines: "North Indian, Biryani,....",
+    rating: 4.4,
+    time: 35,
+  },
+  {
+    resName: "KFC",
+    cuisines: "Burger, Biryani,....",
+    rating: 4.3,
+    time: 30,
+  },
+  {
+    resName: "Punjabi Foods",
+    cuisines: "North Indian, Home Food....",
+    rating: 4.2,
+    time: 25,
+  },
+  {
+    resName: "Roti wala",
+    cuisines: "North Thali, Home Food,....",
+    rating: 4.1,
+    time: 38,
+  },
+  {
+    resName: "Biryani Foods",
+    cuisines: "Biryani,....",
+    rating: 3.4,
+    time: 50,
+  },
+  {
+    resName: "Meghana Foods",
+    cuisines: "North Indian, Biryani,....",
+    rating: 4.4,
+    time: 35,
+  },
+  {
+    resName: "KFC",
+    cuisines: "Burger, Biryani,....",
+    rating: 4.3,
+    time: 30,
+  },
+  {
+    resName: "Punjabi Foods",
+    cuisines: "North Indian, Home Food....",
+    rating: 4.2,
+    time: 25,
+  },
+  {
+    resName: "Roti wala",
+    cuisines: "North Thali, Home Food,....",
+    rating: 4.1,
+    time: 38,
+  },
+  {
+    resName: "Biryani Foods",
+    cuisines: "Biryani,....",
+    rating: 3.4,
+    time: 50,
+  },
+];
+
+export default resList;
